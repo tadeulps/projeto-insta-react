95da670370809cce58abc99f0919f64b715e756b
thiagoribb <thiribeiro142@gmail.com>
2021-05-04 17:02:00 -0300

    Preparando review

--- src/PostConteudo.js
+++
@@ -1,7 +0,0 @@
-export default function PostConteudo(props){
-    return(
-        <div class="conteudo">
-             <img src={`assets/img/${props.conteudo}.svg`} />
-        </div>
-    );
-}
--- src/Posts.js
+++
@@ -1,9 +0,0 @@
-import PostTopo from "./PostTopo"
-import Post from "./Post"
-export default function Posts(){
-    return(
-     <div class="posts">
-        <Post/>
-     </div>
-    )
-}
